c5499127334eb5b1d89bb68aab390ee0da1a61b9
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-27 22:45:07 -0700

    Added test for new

--- test/spec/queue.test.js
+++ test/spec/queue.test.js
@@ -4,6 +4,38 @@ var nextTick = require('next-tick');
 var Queue = require('../..');
 
 describe('Queue', function () {
+  it('infinite parallelism (not new)', function (done) {
+    var queue = Queue();
+
+    var results = [];
+    queue.defer(function (callback) {
+      results.push('1.0');
+      nextTick(function () {
+        results.push('1.1');
+        callback();
+      });
+    });
+    queue.defer(function (callback) {
+      results.push('2.0');
+      nextTick(function () {
+        results.push('2.1');
+        callback();
+      });
+    });
+    queue.defer(function (callback) {
+      results.push('3.0');
+      nextTick(function () {
+        results.push('3.1');
+        callback();
+      });
+    });
+    queue.await(function (err) {
+      assert.ok(!err, 'No errors');
+      assert.deepEqual(results, ['1.0', '2.0', '3.0', '1.1', '2.1', '3.1']);
+      done();
+    });
+  });
+
   it('infinite parallelism', function (done) {
     var queue = new Queue();
 
