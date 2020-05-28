1ab599d2f0fb38486cb8dc580a8826302399597a
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-27 22:42:50 -0700

    Added performance tests

--- test/spec/performance.test.js
+++ test/spec/performance.test.js
@@ -2,11 +2,39 @@ var nextTick = require('next-tick');
 
 var Queue = require('../..');
 
-var MAX_STACK = 10000;
+var MAX_STACK = 100000;
 
 describe('performance', function () {
-  it('stack overflow', function (done) {
+  it('stack overflow (parallism 1)', function (done) {
     var queue = Queue(1);
+
+    queue.defer(function (callback) {
+      nextTick(callback);
+    });
+    for (var index = 0; index < MAX_STACK; index++) {
+      queue.defer(function (callback) {
+        callback();
+      });
+    }
+    queue.await(done);
+  });
+
+  it('stack overflow (parallism 2)', function (done) {
+    var queue = Queue(2);
+
+    queue.defer(function (callback) {
+      nextTick(callback);
+    });
+    for (var index = 0; index < MAX_STACK; index++) {
+      queue.defer(function (callback) {
+        callback();
+      });
+    }
+    queue.await(done);
+  });
+
+  it('stack overflow (parallism Infinity)', function (done) {
+    var queue = Queue();
 
     queue.defer(function (callback) {
       nextTick(callback);
