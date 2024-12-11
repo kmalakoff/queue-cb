785b8397fde41bfb97319774200662453f998d3c
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-10 18:09:23 -0800

    Upgrade modules

--- dist/cjs/LinkedArray.js
+++ dist/cjs/LinkedArray.js
@@ -14,20 +14,6 @@ function _class_call_check(instance, Constructor) {
         throw new TypeError("Cannot call a class as a function");
     }
 }
-function _defineProperties(target, props) {
-    for(var i = 0; i < props.length; i++){
-        var descriptor = props[i];
-        descriptor.enumerable = descriptor.enumerable || false;
-        descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
-        Object.defineProperty(target, descriptor.key, descriptor);
-    }
-}
-function _create_class(Constructor, protoProps, staticProps) {
-    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) _defineProperties(Constructor, staticProps);
-    return Constructor;
-}
 var Node = /*#__PURE__*/ function() {
     "use strict";
     function Node(val) {
@@ -35,16 +21,12 @@ var Node = /*#__PURE__*/ function() {
         this.prev = this.next = this;
         this.value = val;
     }
-    _create_class(Node, [
-        {
-            key: "link",
-            value: function link(next) {
-                this.next = next;
-                next.prev = this;
-                return next;
-            }
-        }
-    ]);
+    var _proto = Node.prototype;
+    _proto.link = function link(next) {
+        this.next = next;
+        next.prev = this;
+        return next;
+    };
     return Node;
 }();
 var LinkedArray = /*#__PURE__*/ function() {
@@ -54,33 +36,26 @@ var LinkedArray = /*#__PURE__*/ function() {
         this.node = null;
         this.length = 0;
     }
-    _create_class(LinkedArray, [
-        {
-            key: "push",
-            value: function push(value) {
-                var node = new Node(value);
-                this.length++;
-                if (!this.node) {
-                    this.node = node;
-                    return node;
-                }
-                this.node.prev.link(node);
-                node.link(this.node);
-                return node;
-            }
-        },
-        {
-            key: "shift",
-            value: function shift() {
-                if (!this.node) throw new Error("Cannot shift: array empty");
-                var node = this.node;
-                this.length--;
-                node.prev.link(node.next);
-                if (node === this.node) this.node = node.next === node ? null : node.next;
-                return node.link(node).value;
-            }
+    var _proto = LinkedArray.prototype;
+    _proto.push = function push(value) {
+        var node = new Node(value);
+        this.length++;
+        if (!this.node) {
+            this.node = node;
+            return node;
         }
-    ]);
+        this.node.prev.link(node);
+        node.link(this.node);
+        return node;
+    };
+    _proto.shift = function shift() {
+        if (!this.node) throw new Error('Cannot shift: array empty');
+        var node = this.node;
+        this.length--;
+        node.prev.link(node.next);
+        if (node === this.node) this.node = node.next === node ? null : node.next;
+        return node.link(node).value;
+    };
     return LinkedArray;
 }();
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
