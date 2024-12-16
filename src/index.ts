01764704f817f386dbeed0b1ce40aa0573a8ed9c
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-15 20:30:27 -0800

    Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import LinkedArray from './LinkedArray.mjs';
+// @ts-ignore
+import LinkedArray from './LinkedArray.ts';
 
-export default function Queue(parallelism) {
-  if (typeof parallelism === 'undefined') parallelism = Infinity;
+export type DeferCallback = (error: Error | null, result: unknown | undefined) => void;
+export type DeferFunction = (calback: DeferCallback) => void;
+export type AwaitCallback = (error: Error | null) => void;
 
+export default function Queue(parallelism: number = Infinity) {
   let awaitCalled = false;
-  let awaitCallback = null;
+  let awaitCallback: AwaitCallback | null = null;
+
   function callAwait() {
     if (awaitCalled || !awaitCallback) return;
     awaitCalled = true;
@@ -24,14 +28,14 @@ export default function Queue(parallelism) {
   }
 
   return {
-    defer: function defer(deferFn) {
+    defer(defer: DeferFunction) {
       if (error) return;
       if (runningCount < parallelism) {
         runningCount++;
-        deferFn(queueCallback);
-      } else tasks.push(deferFn);
+        defer(queueCallback);
+      } else tasks.push(defer);
     },
-    await: function awaitFn(callback) {
+    await(callback: AwaitCallback) {
       if (awaitCallback) throw new Error(`Awaiting callback was added twice: ${callback}`);
       awaitCallback = callback;
       if (error || !(tasks.length + runningCount)) return callAwait();
