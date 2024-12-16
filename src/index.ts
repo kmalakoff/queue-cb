224032ef8b3d51b6d546708d0a66690924af21ae
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-15 21:00:25 -0800

    Convert to a class

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,56 @@
 // @ts-ignore
 import LinkedArray from './LinkedArray.ts';
 
-export type DeferCallback = (error: Error | null, result: unknown | undefined) => void;
+export type DeferCallback = (error: Error | null) => void;
 export type DeferFunction = (calback: DeferCallback) => void;
 export type AwaitCallback = (error: Error | null) => void;
 
-export default function Queue(parallelism: number = Infinity) {
-  let awaitCalled = false;
-  let awaitCallback: AwaitCallback | null = null;
+type CallAwaitFunction = () => void;
+type CallDeferFunction = (error: Error | null) => void;
 
-  function callAwait() {
-    if (awaitCalled || !awaitCallback) return;
-    awaitCalled = true;
-    return awaitCallback(error);
+export default class Queue {
+  private parallelism: number;
+  private awaitCallback: AwaitCallback | null;
+  private tasks: LinkedArray;
+  private runningCount: number;
+  private error: Error | null;
+  private callAwait: CallAwaitFunction;
+  private callDefer: CallDeferFunction;
+
+  constructor(parallelism: number = Infinity) {
+    this.parallelism = parallelism;
+    this.awaitCallback = null;
+    this.tasks = new LinkedArray();
+    this.runningCount = 0;
+    this.error = null;
+
+    let awaitCalled = false;
+    this.callAwait = function callAwait() {
+      if (awaitCalled || !this.awaitCallback) return;
+      awaitCalled = true;
+      return this.awaitCallback(this.error);
+    }.bind(this);
+    this.callDefer = function callDefer(err) {
+      this.runningCount--;
+      if (err && !this.error) this.error = err;
+      if (this.error || !(this.tasks.length + this.runningCount)) return this.callAwait();
+      if (!this.tasks.length) return;
+      this.runningCount++;
+      this.tasks.shift()(this.callDefer);
+    }.bind(this);
   }
 
-  const tasks = new LinkedArray();
-  let runningCount = 0;
-  let error = null;
-  function queueCallback(err) {
-    runningCount--;
-    if (err && !error) error = err;
-    if (error || !(tasks.length + runningCount)) return callAwait();
-    if (!tasks.length) return;
-    runningCount++;
-    tasks.shift()(queueCallback);
+  defer(defer: DeferFunction) {
+    if (this.error) return;
+    if (this.runningCount < this.parallelism) {
+      this.runningCount++;
+      defer(this.callDefer);
+    } else this.tasks.push(defer);
   }
 
-  return {
-    defer(defer: DeferFunction) {
-      if (error) return;
-      if (runningCount < parallelism) {
-        runningCount++;
-        defer(queueCallback);
-      } else tasks.push(defer);
-    },
-    await(callback: AwaitCallback) {
-      if (awaitCallback) throw new Error(`Awaiting callback was added twice: ${callback}`);
-      awaitCallback = callback;
-      if (error || !(tasks.length + runningCount)) return callAwait();
-    },
-  };
+  await(callback: AwaitCallback) {
+    if (this.awaitCallback) throw new Error(`Awaiting callback was added twice: ${callback}`);
+    this.awaitCallback = callback;
+    if (this.error || !(this.tasks.length + this.runningCount)) return this.callAwait();
+  }
 }
